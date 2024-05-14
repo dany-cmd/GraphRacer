457b39a087748cd2872bbe67e68363c3cd656998
bwillecke3 <bwillecke3@gatech.edu>
2024-05-14 20:42:34 +0200

    change generator to generate single json file

--- machine_status_generator.py
+++ machine_status_generator.py
@@ -9,7 +9,7 @@ class MachineStatusGenerator:
         self.MACHINE_DATA_PATH = self.DATA_FOLDER + "/machines.json"
         self.ORDERS_DATA_PATH = self.DATA_FOLDER + "/orders.json"
         self.TRACKING_DATA_PATH = self.DATA_FOLDER + "/trackingresults.json"
-        self.OUTPUT_DATA_PATH = "./output_data/machine_status"
+        self.OUTPUT_DATA_PATH = "./output_data/machine_status.json"
 
         self.MACHINE_DATA = pd.read_json(self.MACHINE_DATA_PATH)
         self.ORDERS_DATA = pd.read_json(self.ORDERS_DATA_PATH)
@@ -20,9 +20,13 @@ class MachineStatusGenerator:
         self.START_TIME = datetime.datetime.strptime("00:00:00", '%H:%M:%S')
         self.END_TIME = datetime.datetime.strptime("00:04:00", '%H:%M:%S')
 
-        self.status = self.build_init_status()
+        self.prev_start_time = None
+        self.prev_end_time = None
+
+        self.machines = self.build_init_machine_status()
+        self.status = {}
     
-    def build_init_status(self):
+    def build_init_machine_status(self):
         machines = []
         for index, machine in self.MACHINE_DATA.iterrows():
             machines.append({"MachineName": machine["MachineName"],
@@ -39,17 +43,34 @@ class MachineStatusGenerator:
     def generate_machine_status_per_time_step(self):
         while self.START_TIME < self.END_TIME:
             current_time_step_end = self.START_TIME + datetime.timedelta(seconds=10)
+            time_stamp = self.START_TIME.strftime("%H%M%S") + '_' + current_time_step_end.strftime("%H%M%S")
+            self.status[time_stamp] = self.machines
             print(self.START_TIME.strftime("%H:%M:%S"), " ", current_time_step_end.strftime("%H:%M:%S"))
-            current_active_pallets = self.get_filtered_tracking_status(self.START_TIME, current_time_step_end)
-            for index, pallet in current_active_pallets.iterrows():
-                for i in range(len(self.status)):
-                    if self.status[i]["StationNumber"] == pallet["station"]:
-                        self.status[i]["pallets"][pallet["order_id"]] =  {"pallet_id": pallet["order_id"]}
+            if self.prev_end_time and self.prev_start_time:
+                self.remove_pallets_from_status()
+            self.add_pallets_to_status(current_time_step_end, time_stamp)
             
-            with open(self.OUTPUT_DATA_PATH + '_' + self.START_TIME.strftime("%H%M%S") + '_' + current_time_step_end.strftime("%H%M%S") + '.json', 'w') as f:
-                json.dump(self.status, f)
+            self.prev_start_time = self.START_TIME
+            self.prev_end_time = current_time_step_end
             self.START_TIME = current_time_step_end
-        return
+        self.write_status_to_file()
+    
+    def remove_pallets_from_status(self):
+        removed_pallets = []
+
+        return removed_pallets
+    
+    def add_pallets_to_status(self, current_time_step_end, time_stamp):
+        current_active_pallets = self.get_filtered_tracking_status(self.START_TIME, current_time_step_end)
+        for index, pallet in current_active_pallets.iterrows():
+            for i in range(len(self.status[time_stamp])):
+                if self.status[time_stamp][i]["StationNumber"] == pallet["station"]:
+                    print(pallet)
+                    self.status[time_stamp][i]["pallets"][pallet["order_id"]] = {"pallet_id": pallet["order_id"],
+                                                                        "in_station_since": str(pallet["StartTime"])}
+    def write_status_to_file(self):
+        with open(self.OUTPUT_DATA_PATH, 'w') as f:
+                json.dump(self.status, f)
 
     def get_filtered_tracking_status(self, start, end):
         mask = (start < self.TRACKING_DATA["StartTime"]) & (self.TRACKING_DATA["StartTime"] <= end) & (self.TRACKING_DATA["EndTime"] >= end)
