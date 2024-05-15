d413cc6d0fa3f71e012274586cfc3453916ce991
bwillecke3 <bwillecke3@gatech.edu>
2024-05-15 09:36:43 +0200

    add incoming pallet feature

--- machine_status_generator.py
+++ machine_status_generator.py
@@ -117,6 +117,59 @@ class MachineStatusGenerator:
             prev_num = num
         return arr
     
+    def add_expecting_pallets(self):
+        self.current_start_time = self.START_TIME
+        self.current_end_time = None
+        pallet_prev_station = {}
+        for i, order in self.ORDERS_DATA.iterrows():
+            pallet_prev_station[order["order_id"]] = None
+        while self.current_start_time < self.END_TIME:
+            self.current_time_step_end = self.current_start_time + datetime.timedelta(seconds=10)
+            time_stamp = self.current_start_time.strftime(self.TIME_FORMAT) + '-' + self.current_time_step_end.strftime(self.TIME_FORMAT)
+            print(self.current_start_time.strftime(self.TIME_FORMAT), " ", self.current_time_step_end.strftime(self.TIME_FORMAT))
+            
+            #set initially seen station
+            for station_name in self.status[time_stamp].keys():
+                station_number = self.status[time_stamp][station_name]["StationNumber"]
+                for pallet_id in self.status[time_stamp][station_name]["pallets"].keys():
+                    if pallet_prev_station[pallet_id] == None:
+                        pallet_prev_station[pallet_id] = int(station_number)
+            
+
+            for pallet_id in pallet_prev_station.keys():
+                if pallet_prev_station[pallet_id] == None:
+                    continue
+                current_station_name = self.station_number_name_mapping[pallet_prev_station[pallet_id]]
+                print(current_station_name)
+                print(pallet_id)
+                print(self.status[time_stamp][current_station_name]["pallets"].keys())
+                if pallet_id in self.status[time_stamp][current_station_name]["pallets"].keys():
+                    print("skip " + pallet_id)
+                    continue
+                next_station_number = pallet_prev_station[pallet_id] + 1
+                if next_station_number not in self.station_number_name_mapping:
+                    continue
+                next_station_name = self.station_number_name_mapping[next_station_number]
+                if pallet_id in self.status[time_stamp][next_station_name]["pallets"].keys():
+                    pallet_prev_station[pallet_id] = next_station_number
+                else:
+                    #add expecting pallet
+                    print("expecting pallet")
+                    steps = self.get_steps_for_order(pallet_id)
+                    self.status[time_stamp][next_station_name]["pallets"][pallet_id] = {"pallet_id": pallet_id,
+                                                                                                                           "expecting_in_station": str(time_stamp),
+                                                                                                                           "in_station_since": 0,
+                                                                                                                           "needed_stations": steps,
+                                                                                                                           "status": "incoming",
+                                                                                                                           "cumulative_throughput_time": 0,
+                                                                                                                           "too_long_in_station": False,
+                                                                                                                           "station_skipped": False,
+                                                                                                                           "throughput_time_too_low": False}
+                
+            self.prev_start_time = self.current_start_time
+            self.prev_end_time = self.current_time_step_end
+            self.current_start_time = self.current_time_step_end
+    
     def write_status_to_file(self):
         with open(self.OUTPUT_DATA_PATH, 'w') as f:
                 json.dump(self.status, f)
@@ -130,5 +183,6 @@ class MachineStatusGenerator:
 
 generator = MachineStatusGenerator()
 generator.generate_machine_status_per_time_step()
+generator.add_expecting_pallets()
 generator.detect_skipped_stations()
 generator.write_status_to_file()
